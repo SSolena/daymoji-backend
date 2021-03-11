efa6acc462f691354846f484492df72b4ca1fdae
blossun <dnsngu@gmail.com>
2021-03-11 18:15:23 +0900

    feat: Emoji 기본 데이터 추가
    Emoji 테이블에 10개 감정 데이터 셋팅
    불필요한 repository interface 삭제
    
    issue #5

--- src/main/java/dev/solar/daymoji/domain/diary/EmojiRepository.java
+++
@@ -1,6 +0,0 @@
-package dev.solar.daymoji.domain.diary;
-
-import org.springframework.data.jpa.repository.JpaRepository;
-
-public interface EmojiRepository extends JpaRepository<Emoji, Long> {
-}
--- src/main/java/dev/solar/daymoji/domain/diary/TodayEmojiRepository.java
+++
@@ -1,6 +0,0 @@
-package dev.solar.daymoji.domain.diary;
-
-import org.springframework.data.jpa.repository.JpaRepository;
-
-public interface TodayEmojiRepository extends JpaRepository<TodayEmoji, Long> {
-}
