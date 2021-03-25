d0811e5d64ec178c19afe8b506095f37513f5805
blossun <dnsngu@gmail.com>
2021-03-26 02:17:46 +0900

    chore: 테스트 클래스에 테스트용 properties를 사용하도록 선언
    테스트 시 필요한 초기 데이터를 data.sql로 넣어준다.
    주의사항
    - h2 사용 시 문자열 데이터는 더블쿼터가 아니라 싱글쿼터로 넣어줘야 함

--- src/test/java/dev/solar/daymoji/domain/diary/DiaryRepositoryTest.java
+++ src/test/java/dev/solar/daymoji/domain/diary/DiaryRepositoryTest.java
@@ -5,6 +5,7 @@ import org.junit.Test;
 import org.junit.runner.RunWith;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.ActiveProfiles;
 import org.springframework.test.context.junit4.SpringRunner;
 
 import java.time.LocalDateTime;
@@ -12,6 +13,7 @@ import java.util.List;
 
 import static org.assertj.core.api.Assertions.assertThat;
 
+@ActiveProfiles("test")
 @RunWith(SpringRunner.class)
 @SpringBootTest
 public class DiaryRepositoryTest {
--- src/test/java/dev/solar/daymoji/web/diary/DiaryControllerTest.java
+++ src/test/java/dev/solar/daymoji/web/diary/DiaryControllerTest.java
@@ -3,15 +3,20 @@ package dev.solar.daymoji.web.diary;
 import com.fasterxml.jackson.databind.ObjectMapper;
 import dev.solar.daymoji.domain.diary.*;
 import org.junit.Before;
+import org.junit.BeforeClass;
 import org.junit.Test;
 import org.junit.jupiter.api.DisplayName;
 import org.junit.runner.RunWith;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.autoconfigure.web.servlet.AutoConfigureMockMvc;
 import org.springframework.boot.test.context.SpringBootTest;
 import org.springframework.hateoas.MediaTypes;
 import org.springframework.http.HttpHeaders;
 import org.springframework.http.MediaType;
+import org.springframework.test.context.ActiveProfiles;
+import org.springframework.test.context.jdbc.Sql;
 import org.springframework.test.context.junit4.SpringRunner;
 import org.springframework.test.web.servlet.MockMvc;
 import org.springframework.test.web.servlet.MvcResult;
@@ -28,10 +33,13 @@ import static org.springframework.test.web.servlet.request.MockMvcRequestBuilder
 import static org.springframework.test.web.servlet.result.MockMvcResultHandlers.print;
 import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.*;
 
+@ActiveProfiles("test")
 @RunWith(SpringRunner.class)
 @SpringBootTest
 @AutoConfigureMockMvc
 public class DiaryControllerTest {
+
+    private static final Logger log = LoggerFactory.getLogger(DiaryControllerTest.class);
 
     @Autowired
     private MockMvc mockMvc;
