51676ba85e170cd09c4f5f04d3732e94b700a267
blossun <dnsngu@gmail.com>
2021-03-31 01:21:43 +0900

    hotfix: 배포 스크립트 수정

--- scripts/deploy.sh
+++ scripts/deploy.sh
@@ -9,9 +9,9 @@ cp $REPOSITORY/zip/*.jar $REPOSITORY/
 
 echo "> 현재 구동 중인 애플리케이션 pid 확인"
 
-CURRENT_PID=$(pgrep -fl daymoji-springboot-webservice | grep jar | awk '{print $1}') # (1)
+CURRENT_PID=$(pgrep -fl ${PROJECT_NAME} | awk '{print $1}')
 
-echo "현재 구동 중인 애플리케이션pid: $CURRENT_PID"
+echo "현재 구동 중인 애플리케이션 pid: $CURRENT_PID"
 
 if [ -z "$CURRENT_PID" ]; then
   echo "> 현재 구동 중인 애플리케이션이 없으므로 종료하지 않습니다."
@@ -23,13 +23,13 @@ fi
 
 echo "> 새 애플리케이션 배포"
 
-JAR_NAME=$(ls -tr $REPOSITORY/*.jar | tail -n 1)
+JAR_NAME=$(ls -tr $REPOSITORY/jar/*.jar | tail -n 1)
 
 echo "> JAR NAME: $JAR_NAME"
 
 echo "> $JAR_NAME 에 실행권한 추가"
 
-chmod +x $JAR_NAME # (2)
+chmod +x $JAR_NAME
 
 echo "> $JAR_NAME 실행"
 
